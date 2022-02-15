3924e5c8d7fceffc0a7881c853303154dfff308b
rasmusrb <rasmusbergstroem87@gmail.com>
2022-02-15 16:49:49 -0500

    asd

--- server.py
+++ server.py
@@ -8,18 +8,17 @@ server_address = "192.168.0.16"
 
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind((server_address, server_port))
-
 # Maximum connections allowed
 server_socket.listen(1)
 
-print("[ LISTENING ON PORT:",server_port,"] The server is ready to receive...")
+print("[ LISTENING ON PORT ] " + str(server_port))
 
 while True:
     client_socket, client_address = server_socket.accept()
     
     try:
     	# Prints client ip and receives request
-        print("[ CONNECTION FROM ]" + str(client_address))
+        print("[ CONNECTION FROM ] " + str(client_address))
         message = client_socket.recv(1024) 
         print(message)
     
@@ -36,13 +35,14 @@ while True:
         for i in range(0, len(outputdata)):
             client_socket.send(bytes(outputdata[i].encode()))
         
-        # Closes client socket conntection
-        client_socket.close()
-    
-    	# Error handling for 404 status code
-    except IOError:
+    except FileNotFoundError:
         client_socket.send(bytes(str.encode("HTTP/1.1 404 Not Found\r\n\r\n")))
         client_socket.send(bytes(str.encode("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")))
-        client_socket.close()
-    
+
+    except Exception as err:
+        client_socket.send(bytes(str.encode("HTTP/1.1 400 Bad Request\r\n\r\n")))
+        client_socket.send(bytes(str.encode("<html><head></head><body><h1>400 Bad Request</h1></body></html>\r\n")))
+        print(err)
+    client_socket.close()
+
 server_socket.close()
